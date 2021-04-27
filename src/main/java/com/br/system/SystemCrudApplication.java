d58c0fb91f3f737ac39d5251ca778ea0adade9ad
Agatha Paiva <agathahost@gmail.com>
2021-04-26 23:35:43 -0300

    tela login e controller

---
+++ src/main/java/com/br/system/SystemCrudApplication.java
@@ -0,0 +1,13 @@
+package com.br.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/system/SystemCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
